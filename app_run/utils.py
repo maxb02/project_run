5283aabecc24d5a1118a8c01def1fe3225d1b9c6
maxb02 <maxbondar02@gmail.com>
2025-08-15 19:21:49 +0300

    add challenge run10

--- app_run/utils.py
+++ app_run/utils.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 def award_challenge_if_completed(athlete_id):
     user = get_user_model().objects.get(id=athlete_id)
-    if user.runs(status=Run.Status.FINISHED).count() == 10:
+    if user.runs.filter(status=Run.Status.FINISHED).count() == 10:
         return Challenge.objects.create(athlete_id=athlete_id, full_name=Challenge.NameChoices.RUN10)
 
     return None
